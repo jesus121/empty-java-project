3d19a1618f8438f20b96852a1783a83b0215388e
jesus <jesusbuenoperales@gmail.com>
2018-04-17 20:07:21 +0200

    la practica está sin corregir a la espera de corregirlo

--- src/com/teaching/android/JavaApp.java
+++ src/com/teaching/android/JavaApp.java
@@ -1,5 +1,6 @@
 package com.teaching.android;
 
+import java.util.Random;
 import java.util.Scanner;
 
 /**
@@ -8,14 +9,31 @@ import java.util.Scanner;
 public class JavaApp {
 
     public static void main(String[] args) {
-        System.out.println("Escribe un número"); // Display the string.
-     Scanner keyboard = new Scanner(System.in);
-     int valor = keyboard.nextInt();
+        System.out.println("Indroduce un número del 1 al 10");
+        //Crea un numero rando del 1 al 10
+        Random generator = new Random();
+        int random = generator.nextInt(10) + 1;
+        Scanner keyboard = new Scanner(System.in);
+        int valor = keyboard.nextInt();
 
-     if (valor % 2 ==0 ) {
-         System.out.println("Es par;");
-     }else {
-         System.out.println("Es impar");
-     }
+        if (random > valor) {
+        System.out.println("Mayor!");
+    }
+    {
+            int valor1 = keyboard.nextInt();
+        if ( random < valor1){
+                System.out.println("Menor!");
+    }
+    {
+            int valor2 = keyboard.nextInt();
+        if (random==valor2){
+            System.out.println("Correcto");
+    }else {
+            System.out.println("Has Fallado");
+        }
+
+            }
+        }
+    System.out.println(random);
     }
 }
